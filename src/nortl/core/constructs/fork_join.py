65dd007372abe0a7f7a543a3fa04331ff157bbc1
Florian Koegler <florian.koegler@imms.de>
2026-02-09 14:46:27 +0000

    Remove Condition from Fork/Join, avoids a dead final_state

--- src/nortl/core/constructs/fork_join.py
+++ src/nortl/core/constructs/fork_join.py
@@ -1,7 +1,6 @@
 from types import TracebackType
 from typing import List
 
-from nortl.core.constructs import Condition
 from nortl.core.protocols import EngineProto, ThreadProto, WorkerProto
 
 __all__ = [
@@ -24,7 +23,11 @@ class Fork:
 
         self.checkpoint_worker = self.engine.current_worker
         self.checkpoint_state = self.engine.current_state
+        self.assigned_worker: WorkerProto
+        self.assigned_thread: ThreadProto
 
+    def _allocate_worker(self) -> None:
+        """Allocate a worker and create thread."""
         free_workers: List[WorkerProto] = [w for w in self.engine.workers.values() if not w.working]
 
         if free_workers == []:
@@ -32,42 +35,43 @@ class Fork:
         else:
             self.assigned_worker = free_workers[0]
 
-        self.new_thread = self.assigned_worker.create_thread(self.threadname)
-
-        self.engine.current_worker = self.assigned_worker
+        self.assigned_thread = self.assigned_worker.create_thread(self.threadname)
 
     def __enter__(self) -> ThreadProto:
         """Create a new thread and integrate it into next free worker (or create a new worker)."""
+        self._allocate_worker()
 
+        # Switch to worker + state
+        self.engine.current_worker = self.assigned_worker
         self.engine.current_state = self.assigned_worker.reset_state
 
         select_id = len(self.assigned_worker.threads)
 
-        condition = Condition(self.engine, self.assigned_worker.start & (self.assigned_worker.select == select_id))
+        # Create start transition from worker idle into fork body
+        self.engine.jump_if(self.assigned_worker.start & (self.assigned_worker.select == select_id), self.engine.next_state)
+        self.engine.current_state = self.engine.next_state
 
-        with condition:
-            state_within_condition = self.engine.current_state
-            state_within_condition.set_metadata('Fork Select ID', select_id)
-            self.engine.set(self.assigned_worker.idle, 0)
+        self.engine.set(self.assigned_worker.idle, 0)
 
         for worker in self.engine.workers.values():
             if worker is self.checkpoint_worker:
                 spawning_worker = worker
 
         spawning_thread = spawning_worker.threads[-1]
-        spawning_thread._spawned_threads.append(self.new_thread)
+        spawning_thread._spawned_threads.append(self.assigned_thread)
 
         # Adjust signal accesses
         self.engine.signal_manager.free_accesses_from_thread(spawning_thread)
         self.engine.scratch_manager.free_accesses_from_thread(spawning_thread)
 
-        self.engine.current_state = state_within_condition
+        # Add metadata
+        self.engine.current_state.set_metadata('Fork Select ID', select_id)
 
         self.engine.sync()
 
-        self.new_thread.active = True
+        self.assigned_thread.active = True
 
-        return self.new_thread
+        return self.assigned_thread
 
     def __exit__(
         self,
@@ -75,13 +79,16 @@ class Fork:
         exc_val: BaseException | None,
         exc_tb: TracebackType | None,
     ) -> None:
+        # Finalize fork body
         self.engine.sync()
-        self.new_thread.finish()
+        self.assigned_thread.finish()
 
+        # Return to old worker + state
         self.engine.current_worker = self.checkpoint_worker
         self.engine.current_state = self.checkpoint_state
 
         thread_id = len(self.assigned_worker.threads)
+
         # Now the thread has been rendered and so we start the Worker!
         self.engine.set(self.assigned_worker.start, 1)
         self.engine.set(self.assigned_worker.select, thread_id)
